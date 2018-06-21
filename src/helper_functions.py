ae4de26700e3034ed9f07b2b3ad642fe6f1ba519
Greg Hovhannisyan <ghovhannisyan@ucdavis.edu>
2018-06-20 17:19:59 -0700

    folder change

---
+++ src/helper_functions.py
@@ -0,0 +1,46 @@
+from torch.utils.data import DataLoader
+
+
+
+
+
+
+
+
+
+# This will define the 4 (or n) point mask applied to the image.
+class mask_state():
+
+# The state will be represented as 8 points in a vector. It will go as: x1, y1, x2, y2, x3, y3, x4, y4
+    def __init__(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.x4 = x4
+        self.y4 = y4
+
+
+
+
+
+
+
+
+
+
+def combine_cnn_with_state(cnn_feature_vector, state_vector):
+    # Takes as input the feature vector from the cnn and combines it with the feature vector from the state
+
+
+
+
+
+def get_reward(old_iou, new_iou):
+    # Takes as input the intersection over union at time t and t+1. Returns +sign(new_iou - old_iou)
+
+
+
+
---
+++ src/main_rnn.py
@@ -0,0 +1,3 @@
+# Takes as input a vector of state+image and outputs two vectors.
+# One vector defines which action to take
+# The other vector defines how much action to take
