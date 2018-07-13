eec8d9d6dc03e489c58f942a4c2059f1e6e44a1a
Greg Hovhannisyan <ghovhannisyan@ucdavis.edu>
2018-07-12 21:37:24 -0700

    Resized images and polygons. Added some new helpful functions and did some organizing.

---
+++ src/adjust_polygons_after_cropping.py
@@ -0,0 +1,40 @@
+# After the images have been cropped from the original images, we need to resize the
+
+
+
+from pycocotools.coco import COCO
+import json
+from .resize_functions import adjust_poly_crop
+
+read_directory = '/media/greghovhannisyan/BackupData1/mscoco/images/train2017_crop_bbox/'
+write_file_path = '/home/greghovhannisyan/PycharmProjects/towards_rlnn_cnn/ObjectSegWithRL/data/train_2017_bbox_crop_polygons_adjusted.json'
+annFile = '/media/greghovhannisyan/BackupData1/mscoco/annotations/instances/instances_train2017.json'
+vertex_json_path = '/home/greghovhannisyan/PycharmProjects/towards_rlnn_cnn/ObjectSegWithRL/data/vertex_count_segmentation_ids.json'
+bbox_json_path = '/home/greghovhannisyan/PycharmProjects/towards_rlnn_cnn/ObjectSegWithRL/data/bbox_crop_shape_gte10k.json'
+
+# initialize COCO api for instance annotations
+coco=COCO(annFile)
+
+with open(vertex_json_path, 'r') as read_vertex:
+    shape_json = json.load(read_vertex)
+
+combined_shape_list = list()
+# combine the shape_json
+for key in shape_json:
+    combined_shape_list.extend(shape_json[key])
+
+seg_anns = coco.loadAnns(combined_shape_list)
+
+new_poly = dict()
+for ann in seg_anns:
+    #print(str(ann['id']) + "yes")
+    new_poly[ann['id']] = adjust_poly_crop(ann['bbox'], ann['segmentation'][0])
+
+with open(write_file_path, 'w') as write_file:
+    json.dump(new_poly, write_file, sort_keys=True, indent=4)
+
+
+
+
+
+
--- src/resize_functions.py
+++ src/resize_functions.py
@@ -4,10 +4,115 @@
 
 import skimage.io as io
 import matplotlib.pyplot as plt
-import numpy as np
 from skimage.transform import resize
 import json
 
+# def import_resize_functions_jup():
+#     import sys
+#     import os
+#     sys.path.insert(0, os.path.abspath('..'))
+#
+#     from resize_functions import *
+
+# Resize image and filter as a batch.
+def resize_img_and_poly_dir(image_directory, polygon_json, filter_set, new_shape, image_write_directory,
+                            annotation_write_path):
+
+    new_poly_dict = dict()
+
+    for seg_id in filter_set:
+        temp_path = image_directory + str(seg_id) + '.jpg'
+        temp_img, temp_poly = resize_img_and_poly(temp_path, new_shape, polygon_json[str(seg_id)])
+        new_poly_dict[seg_id] = temp_poly
+        io.imsave(image_write_directory + str(seg_id) + '.jpg', temp_img)
+
+    with open(annotation_write_path, 'w') as write_file:
+        json.dump(new_poly_dict, write_file, indent=4)
+
+
+# Resize the image and resize the polygon associated with it
+def resize_img_and_poly(image_file_path, new_shape, polygon):
+    read_img = io.imread(image_file_path)
+    height_old, width_old = get_height_width(read_img)
+    height_new, width_new = new_shape
+
+    x_ratio = width_new / width_old
+    y_ratio = height_new / height_old
+
+    new_img = resize(read_img, new_shape)
+
+    new_poly = list()
+
+    for i in range(0, len(polygon), 2):
+        new_poly.append(polygon[i] * x_ratio)
+        new_poly.append(polygon[i + 1] * y_ratio)
+
+    adjust_poly_out_of_bounds(new_poly, width_new, width_old)
+
+    return new_img, new_poly
+
+
+def get_set_from_json(input_json):
+    return_set = set()
+    for key in input_json:
+        for val in input_json[key]:
+            return_set.add(val)
+
+    return return_set
+
+def get_key_with_most_vals(in_dict):
+    maxcount = max(len(v) for v in in_dict.values())
+    return [k for k, v in in_dict.items() if len(v) == maxcount]
+
+
+# Load the image and put the annotation on it. Then display it.
+def show_image_with_mask(coco_instance, image_np_arr, annotation):
+    # Commented code, just in case, the image is provided as a path and not a np_array
+    #image = io.imread(image_np_arr)
+    #plt.imshow(image)
+
+    plt.imshow(image_np_arr)
+    coco_instance.showAnns(annotation)
+
+# If we only have one segmentation id, then we need to make a list and add it to the list.
+# All of the ids must be integer. not string
+# seg_id is either a list of a single variable
+def check_annotation_id(seg_id):
+    if(type(seg_id) is list):
+        return list
+    else:
+        if(type(seg_id) is str):
+            return [int(seg_id)]
+        else:
+            return seg_id
+
+# Given a polygon, we are going to return a new polygon that has the number_of_vertices vertices.
+# Need to account for the new length being multiple times the current length.
+# In this case, will need to make a new vertex between at least 1 newly created vertex.
+# For example, if current_n = 4 and desired_n = 10, then we need to get v1.5 = add_vertex(v1,v2) and add_vertex(v1.5,v2)
+
+def add_vertices_to_polygon(polygon, number_of_vertices):
+    # If its already of the proper length, then just return it
+    if((len(polygon)/2) == number_of_vertices):
+        return polygon
+    # If it is not of the desired length
+    else:
+
+
+
+
+
+# Given two vertices in two dimensional space, returns a new vertex that is between the two points and on the same line.
+def add_vertex(vertex1, vertex2):
+    # Could use distance to calculate the best place to add a new point.
+
+    x1, y1 = vertex1
+    x2, y2 = vertex2
+
+    x3 = (x1 + x2)/2
+    y3 = (y1 + y2)/2
+
+    return (x3, y3)
 
 # Crop the image based on given bounding box and also adjust the dimensions of the polygon
 def crop_and_adjust_poly(image_file_path, bbox, polygon):
@@ -26,26 +131,7 @@ def get_height_width(sci_img):
 
     return height, width
 
-# Resize the image and resize the polygon associated with it
-def resize_img_and_poly(image_file_path, new_shape, polygon):
-    read_img = io.imread(image_file_path)
-    height_old, width_old = get_height_width(read_img)
-    height_new, width_new = new_shape
 
-    x_ratio = width_new/width_old
-    y_ratio = height_new/height_old
-
-    new_img = resize(read_img, new_shape)
-
-    new_poly = list()
-
-    for i in range(0, len(polygon), 2):
-        new_poly.append(polygon[i] * x_ratio)
-        new_poly.append(polygon[i+1] * y_ratio)
-
-    adjust_poly_out_of_bounds(new_poly, width_new, width_old)
-
-    return new_img, new_poly
 
 def adjust_poly_crop(bbox, polygon):
     poly_len = len(polygon)
