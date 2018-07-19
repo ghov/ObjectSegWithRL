a89683ed549b5375ba9671aafdb664318a629201
Greg Hovhannisyan <ghovhannisyan@ucdavis.edu>
2018-07-18 19:58:33 -0700

    Fixed teh move_jpg_and_json_subset file. The annotation and the images did not match with respect to length. Problem with modifying a list while iterating over it. Fixed by making a copy of the list and modifying it, while iterating over the original list.

---
+++ src/greg_cnn.py
@@ -0,0 +1,59 @@
+# Takes as input a 3 channel tensor and returns a vector
+import torch.nn as nn
+
+class GregNet(nn.Module):
+
+    def __init__(self, number_of_vertices):
+        super(GregNet, self).__init__()
+
+        # This computes the cnn section. After this, we need to resize the features into a vector.
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        # This take the vector form of the features and computes the linear layers.
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(2048, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(128, 2 * number_of_vertices),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        #print(x.size())
+        x = x.view(x.size(0), 256 * 6 * 6)
+        #print(x.size())
+        x = self.classifier(x)
+        return x
--- src/pytorch_testing.py
+++ src/pytorch_testing.py
@@ -1,54 +1,66 @@
-from ObjectSegWithRL.src.main_cnn import AlexNet
-from ObjectSegWithRL.src.main_rlnn import GregNet
+from ObjectSegWithRL.src.greg_cnn import GregNet
+#from ObjectSegWithRL.src.main_rlnn import GregNet
 import torch
 
-my_alex = AlexNet()
-my_greg = GregNet(4, 80)
+#my_alex = AlexNet()
+my_greg = GregNet(15)
+#my_greg = GregNet(4, 80)
 #print(torch.zeros(3, 10, 10))
 
-alex_out = my_alex.forward(torch.zeros((3, 224, 224)).unsqueeze_(0))
-
-print(alex_out.size())
-
-greg_out = my_greg.forward(alex_out)
+greg_out = my_greg.forward(torch.zeros((3, 224, 224)).unsqueeze_(0))
 
 print(greg_out.size())
 
-temp_list = (5,8,80)
+#greg_out = my_greg.forward(alex_out)
 
-split_out = torch.split(greg_out, temp_list, dim=1)
+#print(greg_out.size())
 
-actions, amounts, classes = split_out
+#temp_list = (5,8,80)
 
-print(actions.size())
-print(amounts.size())
-print(classes.size())
+#split_out = torch.split(greg_out, temp_list, dim=1)
 
-soft = torch.nn.Softmax(dim=1)
-print(soft(actions))
-print(soft(classes))
+#actions, amounts, classes = split_out
 
-def mixed_loss(old_iou, new_iou, classification_vector):
+# print(actions.size())
+# print(amounts.size())
+# print(classes.size())
+#
+# soft = torch.nn.Softmax(dim=1)
+# print(soft(actions))
+# print(soft(classes))
 
 
 
 
 
-def new_polygon(old_polygon_list, selected_index_int, change_amounts_tuple_int):
-    """
 
-    :param old_polygon_list: a list of x1,y1,x2,y2,... describing the polygon. This should be in matplotlib?
-    :param selected_index_int: the index of the point that the agent chose to move
-    :param change_amounts_tuple_int: the amount of change in x and y for the chosen point
-    :return: the new list, in same format as the input list. This should be in matplotlib?
-    """
 
-    change_x, change_y = change_amounts_tuple_int
-    new_polygon_list = old_polygon_list.copy()
-    new_polygon_list[selected_index_int * 2] = old_polygon_list[selected_index_int * 2] + change_x
-    new_polygon_list[(selected_index_int * 2) + 1] = old_polygon_list[(selected_index_int *2) + 1] + change_y
 
-    return new_polygon_list
+
+
+
+
+#def mixed_loss(old_iou, new_iou, classification_vector):
+
+
+
+
+
+# def new_polygon(old_polygon_list, selected_index_int, change_amounts_tuple_int):
+#     """
+#
+#     :param old_polygon_list: a list of x1,y1,x2,y2,... describing the polygon. This should be in matplotlib?
+#     :param selected_index_int: the index of the point that the agent chose to move
+#     :param change_amounts_tuple_int: the amount of change in x and y for the chosen point
+#     :return: the new list, in same format as the input list. This should be in matplotlib?
+#     """
+#
+#     change_x, change_y = change_amounts_tuple_int
+#     new_polygon_list = old_polygon_list.copy()
+#     new_polygon_list[selected_index_int * 2] = old_polygon_list[selected_index_int * 2] + change_x
+#     new_polygon_list[(selected_index_int * 2) + 1] = old_polygon_list[(selected_index_int *2) + 1] + change_y
+#
+#     return new_polygon_list
 
 
 # Next, need to figure out how to do backprop
