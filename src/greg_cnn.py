Added the dataloader implementation. Added cuda implementation. Added correct loss calculation.

src/greg_cnn.py
@@ -42,10 +42,10 @@ class GregNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(512, 256),
-            nn.ReLU(inplace=True),
+            #nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(256, 128),
-            nn.ReLU(inplace=True),
+            #nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(128, 2 * number_of_vertices),
         )

src/pytorch_stuff.py
@@ -35,7 +35,7 @@ class GregDataset(Dataset):
         poly_np = np.asarray(poly)
         poly_ann = poly_np.astype('float').reshape(-1, len(poly))
 
-        print(seg_id)
+        #print(seg_id)
         # Need to add access to a transform method
         if(self.transform):
             temp_img = self.transform(temp_img)

src/pytorch_training.py
@@ -4,13 +4,15 @@ import torch
 from ObjectSegWithRL.src.greg_cnn import GregNet
 from ObjectSegWithRL.src.pytorch_stuff import GregDataset
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
 cuda_avail = torch.cuda.is_available()
 
 model = GregNet(number_of_vertices=15)
 
-#if cuda_avail:
-#    model.cuda()
+if cuda_avail:
+    model.cuda()
+    #print("yes")
 
 test_transformations = transforms.Compose([
     transforms.ToTensor()
@@ -24,39 +26,55 @@ test_transformations = transforms.Compose([
 
 optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
 
-loss_fn = nn.L1Loss()
+loss_fn = nn.L1Loss().cuda()
 
 annotation_file_path = '/media/greghovhannisyan/BackupData1/mscoco/annotations/by_vertex/30_vertex_poly_adjusted.json'
 root_dir_path = '/media/greghovhannisyan/BackupData1/mscoco/images/by_vertex/30/'
 
 dataset = GregDataset(annotation_file_path, root_dir_path, transform=test_transformations)
 
+my_dataloader = DataLoader(dataset, batch_size=500, num_workers=4)
+
 def train(num_epochs):
     best_acc = 0.0
+    cuda_avail = torch.cuda.is_available()
+
+    if cuda_avail:
+        model.cuda()
+        #print("yes")
 
     for epoch in range(num_epochs):
         model.train()
         train_acc = 0.0
         train_loss = 0.0
-        for i, (images, labels) in enumerate(dataset):
-            print(str(i))
+        for i, (images, labels) in enumerate(my_dataloader):
+            #print(str(i))
             # Move images and labels to gpu if available
             if cuda_avail:
-                images = torch.Tensor(images)
-                labels = torch.Tensor(labels)
-                #images = torch.Tensor(images.cuda())
-                #labels = torch.Tensor(labels.cuda())
+                #print(images.type())
+                #print(labels.type())
+                #print(images.cuda().type())
+                #print(labels.cuda().type())
+
+                #images = torch.Tensor(images)
+                #labels = torch.Tensor(labels)
+                images = torch.Tensor.cuda((images.cuda()))
+                labels = torch.Tensor.cuda((labels.cuda())).float()
 
             # Clear all accumulated gradients
             optimizer.zero_grad()
             # Predict classes using images from the test set
             #torch.unsqueeze(images, 0)
-            outputs = model.forward(torch.unsqueeze(images, 0))
-            #outputs = model.forward(images.unequeze_(0))
+            #print(images.shape)
+
+            outputs = model.forward(images)
+            #outputs = model.forward(torch.unsqueeze(images, 0))
             # Compute the loss based on the predictions and actual labels
-            print(outputs.type())
-            print(labels.type())
-            loss = loss_fn(outputs, labels)
+            #print(outputs.type())
+            #print(labels.type())
+            #print(labels.shape)
+            height, _, width = labels.shape
+            loss = loss_fn(outputs, labels.view(height,30))
             # Backpropagate the loss
             loss.backward()
 
@@ -66,12 +84,16 @@ def train(num_epochs):
             train_loss += loss.cpu().data[0] * images.size(0)
             _, prediction = torch.max(outputs.data, 1)
 
+            #print(outputs.shape)
             #print(prediction.type())
             #print(labels.data.type())
+            #print(labels.shape)
+            #print(prediction.shape)
+            train_acc += torch.sum(outputs.data == labels.data)
             #train_acc += torch.sum(prediction == labels.data)
 
         # Compute the average acc and loss over all 50000 training images
-        #train_acc = train_acc / 5960
+        train_acc = train_acc / 5960
         train_loss = train_loss / 5960
 
         print(epoch)
@@ -82,7 +104,7 @@ def train(num_epochs):
         print("Epoch {}, Train Accuracy: {} , TrainLoss: {}".format(epoch, train_acc, train_loss))
 
 def main():
-    train(1)
+    train(100)
 
 if __name__ == "__main__":
     main()