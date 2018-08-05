39ada406f5f0b314c30603c206bb707ac001c954
Greg Hovhannisyan <ghovhannisyan@ucdavis.edu>
2018-08-05 12:24:32 -0700

    Removed the huge bad reward for having a negative number.

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -220,8 +220,8 @@ def get_np_reward_vector_from_polygon(polygon, change_amount, ground_truth_polyg
     reward_list = get_reward_list_from_iou_list(100, original_iou, iou_list)
 
     # Adjust for the negative_indexes
-    for val in negative_set:
-        reward_list[val] = -100
+    #for val in negative_set:
+    #    reward_list[val] = -100
 
     # Adjust for the step cost if needed
     if(step_cost != None):
