c15f536281c3c34e6bedff8e92f4ccc0de511d7e
Greg Hovhannisyan <ghovhannisyan@ucdavis.edu>
2018-08-07 11:23:26 -0700

    Began work on the tester for the reinforcement learning algorithm.

--- src/reinforce/pytorch_tester.py
+++ src/reinforce/pytorch_tester.py
@@ -1,6 +1,16 @@
 # After having trained a model, we would like to test it on some images.
+# The function will return a list of the consecutive polygons, which can later be used to view the segmentation with
+#  coco.
 
-# Get coco instance
+from ObjectSegWithRL.src.reinforce.greg_cnn import GregNet
+import torch
+
+# The file path for the configuration file
+config_file_path = "/home/greghovhannisyan/PycharmProjects/towards_rlnn_cnn/ObjectSegWithRL/src/reinforce/config.json"
+
+#
+
+
 
 # Get path of image
 
